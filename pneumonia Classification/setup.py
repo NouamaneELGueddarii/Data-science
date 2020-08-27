b0a44fdaefe43ab1ed6cd6ab78f09805248785f9
NouamaneELGueddarii <nouamane-gueddari@outlook.com>
2020-08-27 03:28:53 +0200

    new commit

---
+++ pneumonia Classification/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='an introduction to deep learning using X-Ray images',
+    author='Nouamane El gGueddari',
+    license='MIT',
+)
